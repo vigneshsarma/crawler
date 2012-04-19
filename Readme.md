8ea9fd3bd5a27da048ab5f23e6e7b67be2c8c176
Vignesh Sarma <vignesh.sarma@gmail.com>
2012-04-19 16:52:22 +0530

    complete?

--- Readme.md
+++ Readme.md
@@ -3,7 +3,14 @@
 ###Objective:
   To create a web crawler, to crawl through the links, given a seed page.
 
-###To Do:
-* Use twisted matrix to get web pages, parallely from a list.
+###Features :
+* twisted matrix to get web pages, parallely from a list.
 * Parse the webpages to get links.
-* Store in mongo db.
+
+###Usage :
+```
+Usage: main.py <seed-url>
+Ex: main.py http://www.python.org
+For more options look at 'config.py'
+```
+
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-follow_limit = 10
-verbose = False
-max_parallel = 4
-conn_grow = 2
-ignore = ['pdf','xml']
+follow_limit = 100 #this is the maximum number of links it will follow.
+#I tried this up to 800 successfully
+
+verbose = False #if set to true it will print the liks parsed from each page.
+
+max_parallel = 8 #it is the number of parallel http requests that take place.
+#It could be slightly higher depending on conn_grow.
+conn_grow = 2 #It is the grawth rate of connections.
+
+ignore = ['pdf','xml'] #link that end with these strings will be ignored.
