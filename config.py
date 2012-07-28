674dbd6e066aa69e34f1ff04f66b9097ef6bdb54
Vignesh Sarma K <vignesh.sarma@gmail.com>
2012-07-28 13:13:18 +0530

    updated gitignore and config

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-follow_limit = 100 #this is the maximum number of links it will follow.
+follow_limit = 200 #this is the maximum number of links it will follow.
 #I tried this up to 800 successfully
 
 verbose = False #if set to true it will print the liks parsed from each page.
 
-max_parallel = 8 #it is the number of parallel http requests that take place.
+max_parallel = 12#it is the number of parallel http requests that take place.
 #It could be slightly higher depending on conn_grow.
-conn_grow = 2 #It is the grawth rate of connections.
+conn_grow = 3 #It is the grawth rate of connections.
 
-ignore = ['pdf','xml'] #link that end with these strings will be ignored.
+ignore = ['pdf','xml','zip','gz','tar','bz2'] #link that end with these strings will be ignored.
